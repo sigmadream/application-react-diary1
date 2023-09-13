1e78ec7714afbc3f2627027b48efb2421f52b707
Sangkon Han <sigmadream@gmail.com>
2023-09-14 02:29:01 +0900

    feat: 일기 입력
    - 폼 구성
    - 저장을 위한 간단한 함수

---
+++ src/DiaryEditor.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    alert("저장 성공!");
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          value={state.author}
+          onChange={handleChangeState}
+          name="author"
+          placeholder="작성자"
+          type="text"
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={handleChangeState}
+          name="content"
+          placeholder="일기"
+          type="text"
+        />
+      </div>
+      <div>
+        <span>오늘의 감정점수 : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+export default DiaryEditor;
