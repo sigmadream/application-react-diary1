6cd736a11359dcb79d5c17e97a53a9192a527e5d
Sangkon Han <sigmadream@gmail.com>
2023-09-14 02:40:25 +0900

    feat: 일기 목록

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "홍길동",
+    content: "아버지, 아... 아버지",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "춘향이",
+    content: "공무원, 아... 공무원",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "심청이",
+    content: "저는 물이 싫어요",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryItem.js
@@ -0,0 +1,16 @@
+const DiaryItem = ({ id, author, content, emotion, created_date }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span className="author_info">
+          | 작성자 : {author} | 감정점수 : {emotion} |
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+
+export default DiaryItem;
